448a6ca451bb855e8d403b1e76186bf39df75009
Enzo Falcon <falcone134@gmail.com>
2024-08-05 14:18:43 -0500

    Adding new pages

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,4 +1,4 @@
-import { Collection } from 'lib/types';
+import { Collection, Menu, Page } from 'lib/types';
 
 export type SortFilterItem = {
   title: string;
@@ -63,6 +63,79 @@ export const COLLECTIONS: Collection[] = [
     updatedAt: ''
   }
 ];
+
+export const FOOTER_PAGES: Menu[] = [
+  {
+    title: 'Inicio',
+    path: '/'
+  },
+  {
+    title: 'Sobre Nosotros',
+    path: '/about'
+  },
+  {
+    title: 'Términos y Condiciones',
+    path: '/terms-and-conditions'
+  },
+  {
+    title: 'Políticas de Privacidad',
+    path: '/privacy-policy'
+  }
+]
+
+export const PAGES: Page[] = [
+  {
+    id: '1',
+    title: 'Sobre Nosotros',
+    handle: 'about',
+    body: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus. Integer orci tellus, faucibus accumsan commodo vel, imperdiet ac odio. Suspendisse nec interdum urna. Phasellus nec commodo mi. Aliquam a dolor ante. Cras ornare dictum enim quis varius. Nulla porttitor mi ornare rhoncus iaculis. Duis neque dolor, vehicula eget nisi at, molestie congue leo. Vestibulum facilisis urna ac est placerat, a auctor nibh lacinia. Vivamus ut erat sed dui gravida placerat. Sed mollis mauris at elementum ultricies.
+
+      Vestibulum sodales at nulla vel auctor. Nulla ornare pharetra justo, at consequat sapien finibus vitae. Nulla rutrum, justo at aliquet dapibus, enim tellus laoreet ante, eget hendrerit nibh ex nec odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel luctus risus. Aenean ac orci sodales, consectetur nisl sit amet, ornare felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar placerat suscipit. Cras eget interdum turpis. Praesent vel molestie quam, viverra interdum dolor. 
+    `,
+    bodySummary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus.',
+    seo: {
+      title: 'Sobre Nosotros',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus.'
+    },
+    createdAt: '2024-04-12',
+    updatedAt: '2024-04-12'
+  },
+  {
+    id: '2',
+    title: 'Términos y condiciones',
+    handle: 'terms-and-conditions',
+    body: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus. Integer orci tellus, faucibus accumsan commodo vel, imperdiet ac odio. Suspendisse nec interdum urna. Phasellus nec commodo mi. Aliquam a dolor ante. Cras ornare dictum enim quis varius. Nulla porttitor mi ornare rhoncus iaculis. Duis neque dolor, vehicula eget nisi at, molestie congue leo. Vestibulum facilisis urna ac est placerat, a auctor nibh lacinia. Vivamus ut erat sed dui gravida placerat. Sed mollis mauris at elementum ultricies.
+
+      Vestibulum sodales at nulla vel auctor. Nulla ornare pharetra justo, at consequat sapien finibus vitae. Nulla rutrum, justo at aliquet dapibus, enim tellus laoreet ante, eget hendrerit nibh ex nec odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel luctus risus. Aenean ac orci sodales, consectetur nisl sit amet, ornare felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar placerat suscipit. Cras eget interdum turpis. Praesent vel molestie quam, viverra interdum dolor. 
+    `,
+    bodySummary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus.',
+    seo: {
+      title: 'Términos y condiciones',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus.'
+    },
+    createdAt: '2024-04-12',
+    updatedAt: '2024-04-12'
+  },
+  {
+    id: '3',
+    title: 'Políticas de Privacidad',
+    handle: 'privacy-policy',
+    body: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus. Integer orci tellus, faucibus accumsan commodo vel, imperdiet ac odio. Suspendisse nec interdum urna. Phasellus nec commodo mi. Aliquam a dolor ante. Cras ornare dictum enim quis varius. Nulla porttitor mi ornare rhoncus iaculis. Duis neque dolor, vehicula eget nisi at, molestie congue leo. Vestibulum facilisis urna ac est placerat, a auctor nibh lacinia. Vivamus ut erat sed dui gravida placerat. Sed mollis mauris at elementum ultricies.
+
+      Vestibulum sodales at nulla vel auctor. Nulla ornare pharetra justo, at consequat sapien finibus vitae. Nulla rutrum, justo at aliquet dapibus, enim tellus laoreet ante, eget hendrerit nibh ex nec odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel luctus risus. Aenean ac orci sodales, consectetur nisl sit amet, ornare felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar placerat suscipit. Cras eget interdum turpis. Praesent vel molestie quam, viverra interdum dolor. 
+    `,
+    bodySummary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus.',
+    seo: {
+      title: 'Políticas de Privacidad',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida tellus tempus massa euismod auctor. Fusce ut libero et nunc rhoncus mattis sed at risus.'
+    },
+    createdAt: '2024-04-12',
+    updatedAt: '2024-04-12'
+  },
+]
 
 export const HIDDEN_PRODUCT_TAG = 'nextjs-frontend-hidden';
 export const DEFAULT_OPTION = 'Default Title';
--- frontend/lib/shopify/index.ts
+++ frontend/lib/shopify/index.ts
@@ -1,6 +1,6 @@
 import { Cart, Collection, Menu, Page, Product } from 'lib/types';
 import products from '../../app/products.json';
-import { COLLECTIONS } from 'lib/constants'
+import { COLLECTIONS, FOOTER_PAGES, PAGES } from 'lib/constants'
 
 export const CART: Cart = {
   id: 'cartid',
@@ -86,27 +86,14 @@ export async function getMenu(handle: 'frontend-header' | 'frontend-footer'): Pr
         path: '/search/others'
       }
     ],
-    'frontend-footer': [
-      {
-        title: 'About',
-        path: '/about'
-      }
-    ]
+    'frontend-footer': FOOTER_PAGES
   };
 
   return menus[handle] ?? [];
 }
 
-export async function getPage(handle: string): Promise<Page> {
-  return {
-    id: 'about',
-    title: 'About',
-    handle: 'about',
-    body: 'body',
-    bodySummary: 'bodySummary',
-    createdAt: '2024-04-12',
-    updatedAt: '2024-04-12'
-  };
+export async function getPage(handle: string): Promise<Page | undefined> {
+  return PAGES.find((val) => val.handle === handle)
 }
 
 export async function getPages(): Promise<Page[]> {
@@ -165,218 +152,6 @@ export async function getProductRecommendations(productId: string): Promise<Prod
 
   return recommendedProducts.slice(0,5)
 }
-
-/* const PRODUCTS: Product[] = [
-  {
-    id: '1',
-    handle: 'proplan-adult-complete-razas-medianas-alimento-seco-perro',
-    title: 'Proplan Adult Complete Razas Medianas Alimento Seco Perro',
-    description:
-      'Alimento Super Premium para perros adultos desde los 12 meses hasta los 7 años. Con carne fresca de pollo como ingrediente principal y desarrollado con la energía que los perros de raza pequeña necesitan para mantenerse sanos y activos. Junto con una combinación equilibrada de vitaminas y minerales para una piel y pelaje saludable.',
-    images: [
-      {
-        url: 'https://www.superpet.pe/on/demandware.static/-/Sites-SuperPet-master-catalog/default/dwddf261d9/images/-proplan-adulto-complete-3kg.jpg',
-        altText: 'imagen alt',
-        width: 1,
-        height: 1
-      }
-    ],
-    availableForSale: true,
-    descriptionHtml:
-      'Alimento Super Premium para perros adultos desde los 12 meses hasta los 7 años. Con carne fresca de pollo como ingrediente principal y desarrollado con la energía que los perros de raza pequeña necesitan para mantenerse sanos y activos. Junto con una combinación equilibrada de vitaminas y minerales para una piel y pelaje saludable.',
-    priceRange: {
-      maxVariantPrice: {
-        amount: '395.90',
-        currencyCode: 'USD'
-      },
-      minVariantPrice: {
-        amount: '106.90',
-        currencyCode: 'USD'
-      }
-    },
-    options: [
-      {
-        id: 'string',
-        name: 'Peso',
-        values: ['3Kg', '15Kg']
-      }
-    ],
-    featuredImage: {
-      url: 'https://www.superpet.pe/on/demandware.static/-/Sites-SuperPet-master-catalog/default/dwddf261d9/images/-proplan-adulto-complete-3kg.jpg',
-      altText: 'imagen alt',
-      width: 1,
-      height: 1
-    },
-    variants: [
-      {
-        id: '3Kg',
-        title: '3Kg',
-        availableForSale: true,
-        selectedOptions: [
-          {
-            name: 'Peso',
-            value: '3Kg'
-          }
-        ],
-        price: {
-          amount: '106.90',
-          currencyCode: 'USD'
-        }
-      },
-      {
-        id: '15Kg',
-        title: '15Kg',
-        availableForSale: true,
-        selectedOptions: [
-          {
-            name: 'Peso',
-            value: '15Kg'
-          }
-        ],
-        price: {
-          amount: '395.90',
-          currencyCode: 'USD'
-        }
-      }
-    ],
-    seo: {
-      title: '',
-      description: ''
-    },
-    tags: [],
-    updatedAt: ''
-  },
-  {
-    id: '2',
-    handle: 'mio-cane-super-premium-piel-sensible-alimento-seco-perro',
-    title: 'Mio Cane Super Premium Piel Sensible Alimento Seco Perro',
-    description: 'Alimento seco para medicado para perros con piel sensible',
-    images: [
-      {
-        url: 'https://www.superpet.pe/on/demandware.static/-/Sites-SuperPet-master-catalog/default/dwb16b341b/images/-mio-cane-piel-sensible.jpg',
-        altText: 'imagen alt',
-        width: 1,
-        height: 1
-      }, 
-      {
-        url: 'https://www.superpet.pe/on/demandware.static/-/Sites-SuperPet-master-catalog/default/dw91fd527e/images/racion-sp-adps.jpg',
-        altText: '',
-        width: 300,
-        height: 300
-      }
-    ],
-    availableForSale: true,
-    descriptionHtml: `Formulado especialmente para perros con problemas de alergias o intolerancia a otros alimentos. Se utiliza el huevo como principal fuente de proteínas y el arroz como fuente de carbohidratos.
-
-    La albúmina del huevo es una proteína con alto valor nutricional, fácil de asimilar y que no genera alergias.
-    Los carbohidratos del arroz son fáciles de digerir evitando los problemas estomacales.
-    No contiene pescado, maíz o trigo los cuales pueden ser fuentes de alergias.
-    La protección está reforzada con la incorporación de MOS (mananoligosacáridos) los cuales protegen el tracto digestivo del perro y contribuyen con la eliminación de alergias o estrés estomacal.
-    Contiene niveles superiores de zinc y vitamina C los cuales estimulan las defensas del perro protegiéndolo contra todo tipo de estrés.
-    El selenio, la biotina y los ácidos grasos omega 3 y 6 protegen y reparan la piel manteniéndola brillante y saludable.
-    Consulta este y otros productos similares en Alimento Seco para Perro`,
-    priceRange: {
-      maxVariantPrice: {
-        amount: '240.90',
-        currencyCode: 'USD'
-      },
-      minVariantPrice: {
-        amount: '86.90',
-        currencyCode: 'USD'
-      }
-    },
-    options: [
-      {
-        id: 'string',
-        name: 'Peso',
-        values: ['4Kg', '15Kg']
-      }
-    ],
-    featuredImage: {
-      url: 'https://www.superpet.pe/on/demandware.static/-/Sites-SuperPet-master-catalog/default/dwb16b341b/images/-mio-cane-piel-sensible.jpg',
-      altText: 'imagen alt',
-      width: 1,
-      height: 1
-    },
-    variants: [
-      {
-        id: '4Kg',
-        title: '4Kg',
-        availableForSale: true,
-        selectedOptions: [
-          {
-            name: 'Peso',
-            value: '4Kg'
-          }
-        ],
-        price: {
-          amount: '86.90',
-          currencyCode: 'USD'
-        }
-      },
-      {
-        id: '15Kg',
-        title: '15Kg',
-        availableForSale: true,
-        selectedOptions: [
-          {
-            name: 'Peso',
-            value: '15Kg'
-          }
-        ],
-        price: {
-          amount: '240.90',
-          currencyCode: 'USD'
-        }
-      }
-    ],
-    seo: {
-      title: '',
-      description: ''
-    },
-    tags: [],
-    updatedAt: ''
-  },
-  {
-    id: '3',
-    handle: 'proplan-adult-sensitive-adulto-sensible-alimento-seco-perro',
-    availableForSale: true,
-    title: 'Proplan Adult Sensitive Adulto Sensible Alimento Seco Perro',
-    description: 'Alimento seco para perros con piel sensible',
-    descriptionHtml: 'Alimento seco para perros con piel sensible ',
-    options: [
-      {
-        id: 'Peso',
-        name: 'Peso',
-        values: ['3Kg', '15Kg']
-      }
-    ],
-    priceRange: {
-      maxVariantPrice: {
-        amount: '424.90',
-        currencyCode: 'USD'
-      },
-      minVariantPrice: {
-        amount: '115.90',
-        currencyCode: 'USD'
-      }
-    },
-    featuredImage: {
-      url: 'https://www.superpet.pe/on/demandware.static/-/Sites-SuperPet-master-catalog/default/dw5e79ad54/images/-proplan-adulto-sensitive-3kg.jpg',
-      width: 1,
-      height: 1,
-      altText: 'altetxt'
-    },
-    variants: [],
-    images: [],
-    seo: {
-      title: 'xd',
-      description: 'xdescription'
-    },
-    tags: ['tag1', 'tag2'],
-    updatedAt: ''
-  }
-]; */
 
 export async function getProducts({
   query,
